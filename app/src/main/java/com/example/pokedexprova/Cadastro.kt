8bca81b8aac9d147badcafe203ab3581109e9120
JoaoVitorPerin <joao.vperin@outlook.com>
2023-04-25 19:19:09 -0300

    Adicionando persistencia de dados do login

--- app/src/main/java/com/example/pokedexprova/Cadastro.kt
+++ app/src/main/java/com/example/pokedexprova/Cadastro.kt
@@ -1,11 +1,66 @@
 package com.example.pokedexprova
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.learnandroid.loginsqlite.DBHelper
+
 
 class Cadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+        val DB = DBHelper(this)
+        val botao : Button = findViewById<Button>(R.id.botaoCadastrar)
+
+        botao.setOnClickListener(View.OnClickListener {
+            var login = findViewById<EditText>(R.id.cadastroLogin).text.toString()
+            var senha = findViewById<EditText>(R.id.cadastroSenha).text.toString()
+            var senha2 = findViewById<EditText>(R.id.cadastroSenha2).text.toString()
+
+            if (login == "" || senha == "" || senha2 == "") Toast.makeText(
+                this@Cadastro,
+                "POR FAVOR, PREENCHA TODOS OS CAMPOS!",
+                Toast.LENGTH_SHORT
+            ).show() else {
+                if (senha == senha2) {
+                    val conferirUsuario: Boolean = DB.checkusername(login)
+                    if (conferirUsuario == false) {
+                        val insert: Boolean = DB.insertData(login, senha)
+                        if (insert == true) {
+                            findViewById<EditText>(R.id.cadastroLogin).setText("")
+                            findViewById<EditText>(R.id.cadastroSenha).setText("")
+                            findViewById<EditText>(R.id.cadastroSenha2).setText("")
+                            Toast.makeText(
+                                this@Cadastro,
+                                "CADASTRADO COM SUCESSO!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            val intent = Intent(applicationContext, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(
+                                this@Cadastro,
+                                "NÃO FOI POSSÍVEL CADASTRAR!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } else {
+                        Toast.makeText(
+                            this@Cadastro,
+                            "USUARIO JÁ EXISTENTE!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    Toast.makeText(this@Cadastro, "SENHAS NÃO COINCIDEM!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+           })
     }
 }
--- app/src/main/java/com/example/pokedexprova/MainActivity.kt
+++ app/src/main/java/com/example/pokedexprova/MainActivity.kt
@@ -6,23 +6,47 @@
 
 package com.example.pokedexprova
 
+import android.R.attr.password
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.learnandroid.loginsqlite.DBHelper
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val DB = DBHelper(this)
         var button: Button = findViewById<Button>(R.id.botaoLogin)
-        button.setOnClickListener({
-            val intent = Intent(this@MainActivity, TelaPokedex::class.java)
-            startActivity(intent)
+        button.setOnClickListener(View.OnClickListener {
+            var login = findViewById<EditText>(R.id.login).text.toString()
+            var senha = findViewById<EditText>(R.id.senha).text.toString()
+            if (login == "" || senha == "") Toast.makeText(
+                this@MainActivity,
+                "PREENCHA TODOS OS CAMPOS!",
+                Toast.LENGTH_SHORT
+            ).show() else {
+                val conferirLogin: Boolean = DB.checkusernamepassword(login, senha)
+                if (conferirLogin == true) {
+                    Toast.makeText(this@MainActivity, "LOGIN FEITO COM SUCESSO!", Toast.LENGTH_SHORT)
+                        .show()
+                    findViewById<EditText>(R.id.login).setText("")
+                    findViewById<EditText>(R.id.senha).setText("")
+                    val intent = Intent(applicationContext, TelaPokedex::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@MainActivity, "CREDENCIAIS INVÁLIDAS!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
         })
 
-        var button2: Button = findViewById<Button>(R.id.botaoCadastro)
+        var button2: Button = findViewById<Button>(R.id.botaoTelaCadastro)
         button2.setOnClickListener({
             val intent = Intent(this@MainActivity, Cadastro::class.java)
             startActivity(intent)
